f24c2e9f17c625d9901f554b657ff0dc7ee73701
Tejaswini Gundapaneni <52440474+tejaswini972@users.noreply.github.com>
2021-08-03 07:56:38 +0530

    Update configure-webvm2.ps1

--- Hands-on lab/lab-files/ARM-template/configure-webvm2.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm2.ps1
@@ -24,10 +24,10 @@ param (
   $AzureSubscriptionID,
   
       [string]
-    $azuresubscriptionid,
+    $azsubscriptionid,
 
     [string]
-    $azuretenantid
+    $aztenantid
 )
 
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
@@ -217,8 +217,8 @@ $Shortcut.Save()
     
 Replace sub and tenant id
 
-(Get-Content -Path "LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-SUBSCRIPTION-ID", "$azuresubscriptionid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
-(Get-Content -Path "LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-TENANT-ID", "$azuretenantid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
+(Get-Content -Path "LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-SUBSCRIPTION-ID", "$azsubscriptionid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
+(Get-Content -Path "LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-TENANT-ID", "$aztenantid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
 
 
 
