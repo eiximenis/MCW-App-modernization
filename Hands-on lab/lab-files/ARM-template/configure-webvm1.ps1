e2efe7840ff587fb50a40e433a6043532434d3d0
Tejaswini Gundapaneni <52440474+tejaswini972@users.noreply.github.com>
2021-09-08 11:14:02 +0530

    Update configure-webvm1.ps1

--- Hands-on lab/lab-files/ARM-template/configure-webvm1.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm1.ps1
@@ -36,6 +36,28 @@ $vmAdminUsername="demouser"
 $trainerUserName="trainer"
 $trainerUserPassword="Password.!!1"
 
+function Wait-Install {
+    $msiRunning = 1
+    $msiMessage = ""
+    while($msiRunning -ne 0)
+    {
+        try
+        {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute");
+            $Mutex.Dispose();
+            $DST = Get-Date
+            $msiMessage = "An installer is currently running. Please wait...$DST"
+            Write-Host $msiMessage 
+            $msiRunning = 1
+        }
+        catch
+        {
+            $msiRunning = 0
+        }
+        Start-Sleep -Seconds 1
+    }
+}
+
 #Import Common Functions
 $path = pwd
 $path=$path.Path
@@ -44,16 +66,71 @@ $commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1
 
 # Run Imported functions from cloudlabs-windows-functions.ps1
 
-InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
-Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+#CloudLabsManualAgent
 
+CloudLabsManualAgent Install
+
+
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+InstallAzPowerShellModule
+
+
+Function Enable-CloudLabsEmbeddedShadow($vmAdminUsername, $trainerUserName, $trainerUserPassword)
+{
+Write-Host "Enabling CloudLabsEmbeddedShadow"
+#Created Trainer Account and Add to Administrators Group
+$trainerUserPass = $trainerUserPassword | ConvertTo-SecureString -AsPlainText -Force
+
+New-LocalUser -Name $trainerUserName -Password $trainerUserPass -FullName "$trainerUserName" -Description "CloudLabs EmbeddedShadow User" -PasswordNeverExpires
+Add-LocalGroupMember -Group "Administrators" -Member "$trainerUserName"
+
+#Add Windows regitary to enable Shadow
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v Shadow /t REG_DWORD /d 2
+
+#Download Shadow.ps1 and Shadow.xml file in VM
+$drivepath="C:\Users\Public\Documents"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/Shadow.ps1","$drivepath\Shadow.ps1")
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/shadow.xml","$drivepath\shadow.xml")
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/ShadowSession.zip","C:\Packages\ShadowSession.zip")
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/executetaskscheduler.ps1","$drivepath\executetaskscheduler.ps1")
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/shadowshortcut.ps1","$drivepath\shadowshortcut.ps1")
+
+# Unzip Shadow User Session Shortcut to Trainer Desktop
+#$trainerloginuser= "$trainerUserName" + "." + "$($env:ComputerName)"
+#Expand-Archive -LiteralPath 'C:\Packages\ShadowSession.zip' -DestinationPath "C:\Users\$trainerloginuser\Desktop" -Force
+#Expand-Archive -LiteralPath 'C:\Packages\ShadowSession.zip' -DestinationPath "C:\Users\$trainerUserName\Desktop" -Force
+
+#Replace vmAdminUsernameValue with VM Admin UserName in script content 
+(Get-Content -Path "$drivepath\Shadow.ps1") | ForEach-Object {$_ -Replace "vmAdminUsernameValue", "$vmAdminUsername"} | Set-Content -Path "$drivepath\Shadow.ps1"
+(Get-Content -Path "$drivepath\shadow.xml") | ForEach-Object {$_ -Replace "vmAdminUsernameValue", "$trainerUserName"} | Set-Content -Path "$drivepath\shadow.xml"
+(Get-Content -Path "$drivepath\shadow.xml") | ForEach-Object {$_ -Replace "ComputerNameValue", "$($env:ComputerName)"} | Set-Content -Path "$drivepath\shadow.xml"
+(Get-Content -Path "$drivepath\shadowshortcut.ps1") | ForEach-Object {$_ -Replace "vmAdminUsernameValue", "$trainerUserName"} | Set-Content -Path "$drivepath\shadowshortcut.ps1"
+sleep 2
+
+# Scheduled Task to Run Shadow.ps1 AtLogOn
+schtasks.exe /Create /XML $drivepath\shadow.xml /tn Shadowtask
+
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\$trainerUserName" 
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $drivepath\shadowshortcut.ps1 -WindowStyle Hidden"
+Register-ScheduledTask -TaskName "shadowshortcut" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+}
 
 
 # To resolve the error of https://github.com/microsoft/MCW-App-modernization/issues/68. The cause of the error is Powershell by default uses TLS 1.0 to connect to website, but website security requires TLS 1.2. You can change this behavior with running any of the below command to use all protocols. You can also specify single protocol.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
+# Disable IE ESC
+Disable-InternetExplorerESC
+
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
 $branchName = "stage"
+
+# Enable Embedded shadow
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
 
 # Download and extract the starter solution files
 # ZIP File sometimes gets corrupted
@@ -73,33 +150,30 @@ Expand-Archive -LiteralPath "C:\MCW\MCW-App-modernization-stage\Hands-on lab\lab
 ((Get-Content -path C:\inetpub\wwwroot\config.release.json -Raw) -replace 'SETCONNECTIONSTRING',"Server=$SqlIP;Database=PartsUnlimited;User Id=PUWebSite;Password=$SqlPass;") | Set-Content -Path C:\inetpub\wwwroot\config.json
 
 
+#Replace Path
+
+(Get-Content C:\MCW\MCW-App-modernization-stage\'Hands-on lab'\lab-files\ARM-template\webvm-logon-install2.ps1) -replace "replacepath","$Path" | Set-Content C:\MCW\MCW-App-modernization-stage\'Hands-on lab'\lab-files\ARM-template\webvm-logon-install2.ps1 -Verbos
+
+# Schedule Installs for first Logon
+$argument = "-File `"C:\MCW\MCW-App-modernization-stage\Hands-on lab\lab-files\ARM-template\webvm-logon-install2.ps1`""
+$triggerAt = New-ScheduledTaskTrigger -AtLogOn -User demouser
+$action = New-ScheduledTaskAction -Execute "powershell" -Argument $argument 
+Register-ScheduledTask -TaskName "Install Lab Requirements" -Trigger $triggerAt -Action $action -User demouser
+
 # Download and install .NET Core 2.2
 Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/5efd5ee8-4df6-4b99-9feb-87250f1cd09f/552f4b0b0340e447bab2f38331f833c5/dotnet-hosting-2.2.2-win.exe', 'C:\dotnet-hosting-2.2.2-win.exe')
 $pathArgs = {C:\dotnet-hosting-2.2.2-win.exe /Install /Quiet /Norestart /Logs logCore22.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-#Replace Path
-
-(Get-Content C:\MCW\MCW-App-modernization-stage\'Hands-on lab'\lab-files\ARM-template\webvm-logon-install1.ps1) -replace "replacepath","$Path" | Set-Content C:\MCW\MCW-App-modernization-stage\'Hands-on lab'\lab-files\ARM-template\webvm-logon-install1.ps1 -Verbos
-
-# Schedule Installs for first Logon
-$argument = "-File `"C:\MCW\MCW-App-modernization-stage\Hands-on lab\lab-files\ARM-template\webvm-logon-install1.ps1`""
-$triggerAt = New-ScheduledTaskTrigger -AtLogOn -User demouser
-$action = New-ScheduledTaskAction -Execute "powershell" -Argument $argument 
-Register-ScheduledTask -TaskName "Install Lab Requirements" -Trigger $triggerAt -Action $action -User demouser
-
-
+# cred file
 CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
-
     
 Replace sub and tenant id
 
 (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-SUBSCRIPTION-ID", "$azsubscriptionid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
 (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "GET-TENANT-ID", "$aztenantid"} | Set-Content -Path "c:\LabFiles\AzureCreds.txt"
-
 
 #Autologin
 $Username = "demouser"
