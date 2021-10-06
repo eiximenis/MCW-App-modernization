bd3ea5d4a6fe201acdbda0cb586acb5748714079
Tejaswini Gundapaneni <52440474+tejaswini972@users.noreply.github.com>
2021-10-06 17:36:42 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -1,3 +1,8 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension1.txt -Append
+
+$commonscriptpath = "replacepath\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
 function Wait-Install {
     $msiRunning = 1
     $msiMessage = ""
@@ -19,16 +24,7 @@ function Wait-Install {
         Start-Sleep -Seconds 1
     }
 }
-$branchName = "main"
-# Install App Service Migration Assistant
-Wait-Install
-Write-Host "Installing App Service Migration Assistant..."
-Start-Process -file 'C:\AppServiceMigrationAssistant.msi ' -arg '/qn /l*v C:\asma_install.txt' -passthru | wait-process
 
-# Install Edge
-Wait-Install
-Write-Host "Installing Edge..."
-Start-Process -file 'C:\MicrosoftEdgeEnterpriseX64.msi' -arg '/qn /l*v C:\edge_install.txt' -passthru | wait-process
 
 # Install .NET Core 3.1 SDK
 Wait-Install
@@ -36,12 +32,63 @@ Write-Host "Installing .NET Core 3.1 SDK..."
 $pathArgs = {C:\dotnet-sdk-3.1.413-win-x64.exe /Install /Quiet /Norestart /Logs logCore31SDK.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
+$branchName = "prod-1"
+
 # Copy Web Site Files
 Wait-Install
 Write-Host "Copying default website files..."
 Expand-Archive -LiteralPath "C:\MCW\MCW-App-modernization-$branchName\Hands-on lab\lab-files\web-deploy-files.zip" -DestinationPath 'C:\inetpub\wwwroot' -Force
 
-Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
 
 Write-Host "Restarting IIS"
-iisreset.exe /restart
+iisreset.exe /restart 
+
+Write-Host "Re-installed IIS"
+
+#Check if Webvm ip is accessible or not
+Import-Module Az
+
+CD C:\LabFiles
+$credsfilepath = ".\AzureCreds.txt"
+$creds = Get-Content $credsfilepath | Out-String | ConvertFrom-StringData
+$AzureUserName = "$($creds.AzureUserName)"
+$AzurePassword = "$($creds.AzurePassword)"
+$DeploymentID = "$($creds.DeploymentID)"
+$SubscriptionId = "$($creds.AzureSubscriptionID)"
+$passwd = ConvertTo-SecureString $AzurePassword -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AzureUserName, $passwd
+
+Connect-AzAccount -Credential $cred
+
+$vmipdetails=Get-AzPublicIpAddress -ResourceGroupName "hands-on-lab-$DeploymentID" -Name "WebVM-ip" 
+
+$vmip=$vmipdetails.IpAddress
+ 
+$url="http://"+$vmip
+
+$HTTP_Request = [System.Net.WebRequest]::Create($url)
+
+# We then get a response from the site.
+$HTTP_Response = $HTTP_Request.getResponse()
+
+# We then get the HTTP code as an integer.
+$HTTP_Status = [int]$HTTP_Response.StatusCode
+
+If ($HTTP_Status -eq 200){
+    $Validstatus="Succeeded"  ##Failed or Successful at the last step
+    $Validmessage="Post Deployment is successful"
+}
+else{
+    Write-Warning "Validation Failed - see log output"
+    $Validstatus="Failed"  ##Failed or Successful at the last step
+    $Validmessage="Post Deployment Failed"
+
+}
+
+CloudlabsManualAgent setStatus
+
+CloudLabsManualAgent Start
+
+Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
+
+Stop-Transcript
