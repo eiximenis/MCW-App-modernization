10af079fac582313a1c2ab4e1611a91f787fe01f
Tejaswini Gundapaneni <52440474+tejaswini972@users.noreply.github.com>
2021-10-08 16:45:17 +0530

    Update configure-sqlvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
@@ -2,6 +2,8 @@ param (
     [Parameter(Mandatory=$False)] [string] $SqlPass = ""
 )
 
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+
 # Disable Internet Explorer Enhanced Security Configuration
 function Disable-InternetExplorerESC {
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
@@ -75,3 +77,5 @@ function Setup-Sql {
 }
 
 Setup-Sql
+
+Stop-Transcript  
