33ef580cecd013735246ae8b2dce80444951b879
Tejaswini Gundapaneni <52440474+tejaswini972@users.noreply.github.com>
2021-10-25 17:03:17 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -19,7 +19,8 @@ function Wait-Install {
         Start-Sleep -Seconds 1
     }
 }
-$branchName = "main"
+$branchName = "prod-1"
+
 # Install App Service Migration Assistant
 Wait-Install
 Write-Host "Installing App Service Migration Assistant..."
@@ -50,3 +51,50 @@ Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
 # Restart the app for the startup to pick up the database connection string.
 Write-Host "Restarting IIS"
 iisreset.exe /restart
+
+
+#Check if Webvm ip is accessible or not
+Import-Module Az
+
+CD C:\LabFiles
+$credsfilepath = ".\AzureCreds.txt"
+$creds = Get-Content $credsfilepath | Out-String | ConvertFrom-StringData
+$AzureUserName = "$($creds.AzureUserName)"
+$AzurePassword = "$($creds.AzurePassword)"
+$DeploymentID = "$($creds.DeploymentID)"
+$SubscriptionId = "$($creds.AzureSubscriptionID)"
+$passwd = ConvertTo-SecureString $AzurePassword -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AzureUserName, $passwd
+
+Connect-AzAccount -Credential $cred
+
+$vmipdetails=Get-AzPublicIpAddress -ResourceGroupName "hands-on-lab-$DeploymentID" -Name "WebVM-ip" 
+
+$vmip=$vmipdetails.IpAddress
+
+$url="http://"+$vmip
+
+$HTTP_Request = [System.Net.WebRequest]::Create($url)
+
+# We then get a response from the site.
+$HTTP_Response = $HTTP_Request.getResponse()
+
+# We then get the HTTP code as an integer.
+$HTTP_Status = [int]$HTTP_Response.StatusCode
+
+If ($HTTP_Status -eq 200){
+    $Validstatus="Succeeded"  ##Failed or Successful at the last step
+    $Validmessage="Post Deployment is successful"
+}
+else{
+    Write-Warning "Validation Failed - see log output"
+    $Validstatus="Failed"  ##Failed or Successful at the last step
+    $Validmessage="Post Deployment Failed"
+
+}
+
+CloudlabsManualAgent setStatus
+
+CloudLabsManualAgent Start
+
+Stop-Transcript
